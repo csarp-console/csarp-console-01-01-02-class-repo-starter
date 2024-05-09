c81203cf3e116eff74ca43d83c8d57f93b429141
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-05-09 14:28:43 +0200

    A feladat szövege

--- Kreta.Console/Program.cs
+++ Kreta.Console/Program.cs
@@ -1,12 +1,2 @@
-﻿using Kreta.Console.Models.SchoolCitizens;
-
-AwardedStudent tunde = new AwardedStudent("Üveges Tünde", 17, 15000, false);
-Console.WriteLine(tunde);
-AwardedStudent denes = new AwardedStudent("Dolgozo Dénes", 18, 5000, true);
-if (tunde.Age > denes.Age)
-    Console.WriteLine($"{tunde.Name} idősebb, mint {denes.Name}!");
-else if (denes.Age > tunde.Age)
-    Console.WriteLine($"{denes.Name} idősebb, mint {tunde.Name}!");
-else
-    Console.WriteLine("A két diák ugyan annyi éves!");
+﻿Console.WriteLine("A program elinudlt...");
 
